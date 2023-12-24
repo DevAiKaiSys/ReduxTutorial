d64339f8a1f44c998f42592687ef75f5a7e15717
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-12-24 18:50:39 +0700

    Redux Essentials, Part 3: Dispatch postAdded when 'Save Post' is clicked

--- redux-essentials-example/app/ui/posts/AddPostForm.tsx
+++ redux-essentials-example/app/ui/posts/AddPostForm.tsx
@@ -1,14 +1,34 @@
 "use client";
+import { postAdded } from "@/lib/redux/posts/postsSlice";
+import { nanoid } from "@reduxjs/toolkit";
 import React, { ChangeEvent, useState } from "react";
+import { useDispatch } from "react-redux";
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const dispatch = useDispatch();
+
   const onTitleChanged = (e: ChangeEvent<HTMLInputElement>) =>
     setTitle(e.target.value);
   const onContentChanged = (e: ChangeEvent<HTMLTextAreaElement>) =>
     setContent(e.target.value);
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      );
+
+      setTitle("");
+      setContent("");
+    }
+  };
 
   return (
     <section>
@@ -29,7 +49,9 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
       </form>
     </section>
   );
