c56ca4c87a2657952b1073898d85c4d48cd3cf6c
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-17 00:27:33 +0700

    Redux Essentials, Part 4: Adding a Users Slice

--- redux-essentials-example/lib/redux/store.ts
+++ redux-essentials-example/lib/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import postsReducer from "./slices/postsSlice/postsSlice";
+import usersReducer from "./slices/usersSlice/usersSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       posts: postsReducer,
+      users: usersReducer,
     },
   });
 };
