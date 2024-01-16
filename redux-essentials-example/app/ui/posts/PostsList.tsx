9e5982ea3d941e6c52fef80ef2e2c3174b967cad
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-17 01:09:54 +0700

    Redux Essentials, Part 4: Sorting the Posts List

--- redux-essentials-example/app/ui/posts/PostsList.tsx
+++ redux-essentials-example/app/ui/posts/PostsList.tsx
@@ -8,19 +8,27 @@ import { TimeAgo } from "./TimeAgo";
 export const PostsList = () => {
   const posts = useAppSelector((state) => state.posts);
 
-  const renderedPosts = posts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <div>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <Link href={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
-  ));
+  // const renderedPosts = posts.map((post) => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => (b.date ?? "").localeCompare(a.date ?? ""));
+
+  const renderedPosts = orderedPosts.map((post) => {
+    return (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <Link href={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    );
+  });
 
   return (
     <section className="posts-list">
