0a649053ead86193b385b0391aec8920b57cb6d9
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-16 23:18:50 +0700

    Redux Essentials, Part 3: Adding New Posts

--- redux-essentials-example/app/page.tsx
+++ redux-essentials-example/app/page.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "./ui/Navbar";
+import { AddPostForm } from "./ui/posts/AddPostForm";
 import { PostsList } from "./ui/posts/PostsList";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <AddPostForm />
       <PostsList />
     </>
   );
