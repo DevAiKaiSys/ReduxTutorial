a1274e714371d4fcc68bc6638096853ccd206adc
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-19 22:41:40 +0700

    Redux Fundamentals, Part 4: Writing Custom Middleware

--- redux-fundamentals-example-app/app/lib/redux/exampleAddons/middleware.ts
+++ redux-fundamentals-example-app/app/lib/redux/exampleAddons/middleware.ts
@@ -14,3 +14,23 @@ export const print3: Middleware = (storeAPI) => (next) => (action) => {
   console.log("3");
   return next(action);
 };
+
+// function exampleMiddleware(storeAPI) {
+const exampleMiddleware: Middleware = (storeAPI) => {
+  return function wrapDispatch(next) {
+    return function handleAction(action) {
+      // Do anything here: pass the action onwards with next(action),
+      // or restart the pipeline with storeAPI.dispatch(action)
+      // Can also use storeAPI.getState() here
+
+      return next(action);
+    };
+  };
+};
+
+const anotherExampleMiddleware: Middleware =
+  (storeAPI) => (next) => (action) => {
+    // Do something in here, when each action is dispatched
+
+    return next(action);
+  };
