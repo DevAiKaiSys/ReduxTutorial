788d39d1d57d84f55c5bbe6f12dc0e3d479bb8be
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-19 15:45:21 +0700

    Redux Fundamentals, Part 3: Handling Additional Actions

--- redux-fundamentals-example-app/app/lib/redux/reducer.ts
+++ redux-fundamentals-example-app/app/lib/redux/reducer.ts
@@ -1,6 +1,6 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialState: AppState = {
   todos: [
     { id: 0, text: "Learn React", completed: true },
     { id: 1, text: "Learn Redux", completed: false, color: "purple" },
@@ -18,10 +18,7 @@ function nextTodoId(todos: Todo[]): number {
 }
 
 // Use the initialState as a default value
-export default function appReducer(
-  state = initialState,
-  action: PayloadAction
-) {
+export default function appReducer(state = initialState, action: AppAction) {
   // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
     // Do something here based on the different types of actions
@@ -44,6 +41,39 @@ export default function appReducer(
         ],
       };
     }
+    case "todos/todoToggled": {
+      return {
+        // Again copy the entire state object
+        ...state,
+        // This time, we need to make a copy of the old todos array
+        todos: state.todos.map((todo) => {
+          // If this isn't the todo item we're looking for, leave it alone
+          if (todo.id !== action.payload) {
+            return todo;
+          }
+
+          // We've found the todo that has to change. Return a copy:
+          return {
+            ...todo,
+            // Flip the completed flag
+            completed: !todo.completed,
+          };
+        }),
+      };
+    }
+    case "filters/statusFilterChanged": {
+      return {
+        // Copy the whole state
+        ...state,
+        // Overwrite the filters value
+        filters: {
+          // copy the other filter fields
+          ...state.filters,
+          // And replace the status field with the new value
+          status: action.payload,
+        },
+      };
+    }
     default:
       // If this reducer doesn't recognize the action type, or doesn't
       // care about this specific action, return the existing state unchanged
@@ -52,9 +82,23 @@ export default function appReducer(
 }
 
 /* Types */
+type AppState = {
+  todos: Todo[];
+  filters: {
+    status: "All";
+    colors: string[];
+  };
+};
+
 type Todo = {
   id: number;
   text: string;
   completed: boolean;
   color?: string; // Color is optional
 };
+
+// Action types
+type AppAction =
+  | PayloadAction<string, "todos/todoAdded">
+  | PayloadAction<number, "todos/todoToggled">
+  | PayloadAction<string, "filters/statusFilterChanged">;
