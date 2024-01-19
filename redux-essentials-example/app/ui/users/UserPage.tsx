7112e5a485c6a76e74d49492bcb04d227b6b901c
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-19 13:00:43 +0700

    Redux Essentials, Part 8: Selecting Values from Results

--- redux-essentials-example/app/ui/users/UserPage.tsx
+++ redux-essentials-example/app/ui/users/UserPage.tsx
@@ -1,13 +1,12 @@
 "use client";
+import { useGetPostsQuery } from "@/lib/redux/api/apiSlice";
 import { useAppSelector } from "@/lib/redux/hooks";
-import {
-  selectAllPosts,
-  selectPostsByUser,
-} from "@/lib/redux/slices/postsSlice/postsSlice";
+import { Post } from "@/lib/redux/slices/postsSlice/postsSlice";
 import { selectUserById } from "@/lib/redux/slices/usersSlice/usersSlice";
+import { createSelector } from "@reduxjs/toolkit";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useMemo } from "react";
 
 type Props = {};
 
@@ -21,9 +20,31 @@ export const UserPage = ({}: /* match */ Props) => {
   //   const allPosts = selectAllPosts(state);
   //   return allPosts.filter((post) => post.user === userId);
   // });
-  const postsForUser = useAppSelector((state) =>
-    selectPostsByUser(state, userId)
-  );
+  // const postsForUser = useAppSelector((state) =>
+  //   selectPostsByUser(state, userId)
+  // );
+  const selectPostsForUser = useMemo(() => {
+    const emptyArray: Post[] = [];
+    // Return a unique selector instance for this page so that
+    // the filtered results are correctly memoized
+    return createSelector(
+      (res) => res.data,
+      (res, userId: string) => userId,
+      (data: Post[], userId: string) =>
+        data?.filter((post) => post.user === userId) ?? emptyArray
+    );
+  }, []);
+
+  // Use the same posts query, but extract only part of its data
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (result) => ({
+      // We can optionally include the other metadata fields from the result here
+      ...result,
+      // Include a field called `postsForUser` in the hook result object,
+      // which will be a filtered list of posts
+      postsForUser: selectPostsForUser(result, userId),
+    }),
+  });
 
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>
