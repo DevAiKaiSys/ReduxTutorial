274a2913e7c5335061e1d3812aef080b426dd56a
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-01-20 12:33:35 +0700

    Redux Fundamentals, Part 5: Selecting Data in List Items by ID

--- redux-fundamentals-example-app/app/ui/todos/TodoList.tsx
+++ redux-fundamentals-example-app/app/ui/todos/TodoList.tsx
@@ -4,14 +4,20 @@ import TodoListItem from "./TodoListItem";
 import { useSelector } from "react-redux";
 import { ReduxState } from "@/app/lib/redux/store";
 
-const selectTodos = (state: ReduxState) => state.todos;
+// const selectTodos = (state: ReduxState) => state.todos;
+const selectTodoIds = (state: ReduxState) => state.todos.map((todo) => todo.id);
 
 const TodoList = () => {
   // const todos: Todo[] = [];
-  const todos = useSelector(selectTodos);
+  // const todos = useSelector(selectTodos);
 
-  const renderedListItems = todos.map((todo) => {
-    return <TodoListItem key={todo.id} todo={todo} />;
+  // const renderedListItems = todos.map((todo) => {
+  //   return <TodoListItem key={todo.id} todo={todo} />;
+  // });
+  const todoIds = useSelector(selectTodoIds);
+
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />;
   });
 
   return <ul className="todo-list">{renderedListItems}</ul>;
--- redux-fundamentals-example-app/app/ui/todos/TodoListItem.tsx
+++ redux-fundamentals-example-app/app/ui/todos/TodoListItem.tsx
@@ -4,32 +4,96 @@ import {
   capitalize,
 } from "@/app/lib/redux/filtersSlice/colors";
 import React from "react";
-import { Todo } from "@/app/lib/redux/todosSlice/todosSlice";
 // import { ReactComponent as TimesSolid } from "./times-solid.svg"; // error
 import Image from "next/image";
 import svgTimesSolid from "./times-solid.svg";
+import { ReduxState } from "@/app/lib/redux/store";
+import { useDispatch, useSelector } from "react-redux";
 
-type Props = {
-  todo: Todo;
-  onColorChange?: (color: string) => void;
-  onCompletedChange?: (completed: boolean) => void;
-  onDelete?: () => void;
+// type Props = {
+//   todo: Todo;
+//   onColorChange?: (color: string) => void;
+//   onCompletedChange?: (completed: boolean) => void;
+//   onDelete?: () => void;
+// };
+
+// const TodoListItem = ({
+//   todo,
+//   onColorChange,
+//   onCompletedChange,
+//   onDelete,
+// }: Props) => {
+//   const { text, completed, color } = todo;
+
+//   const handleCompletedChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     onCompletedChange && onCompletedChange(e.target.checked);
+//   };
+
+//   const handleColorChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
+//     onColorChange && onColorChange(e.target.value);
+//   };
+
+//   const colorOptions = availableColors.map((c) => (
+//     <option key={c} value={c}>
+//       {capitalize(c)}
+//     </option>
+//   ));
+
+//   return (
+//     <li>
+//       <div className="view">
+//         <div className="segment label">
+//           <input
+//             className="toggle"
+//             type="checkbox"
+//             checked={completed}
+//             onChange={handleCompletedChanged}
+//           />
+//           <div className="todo-text">{text}</div>
+//         </div>
+//         <div className="segment buttons">
+//           <select
+//             className="colorPicker"
+//             value={color}
+//             style={{ color }}
+//             onChange={handleColorChanged}
+//           >
+//             <option value=""></option>
+//             {colorOptions}
+//           </select>
+//           <button className="destroy" onClick={onDelete}>
+//             {/* <TimesSolid /> */}
+//             <Image src={svgTimesSolid} alt="times-solid" />
+//           </button>
+//         </div>
+//       </div>
+//     </li>
+//   );
+// };
+
+const selectTodoById = (state: ReduxState, todoId: number) => {
+  return state.todos.find((todo) => todo.id === todoId);
 };
 
-const TodoListItem = ({
-  todo,
-  onColorChange,
-  onCompletedChange,
-  onDelete,
-}: Props) => {
+type Props = { id: number };
+
+// Destructure `props.id`, since we just need the ID value
+const TodoListItem = ({ id }: Props) => {
+  // Call our `selectTodoById` with the state _and_ the ID value
+  const todo = useSelector((state: ReduxState) => selectTodoById(state, id));
+
+  if (!todo) {
+    return null; // Return early if todo is not found
+  }
+
   const { text, completed, color } = todo;
 
-  const handleCompletedChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onCompletedChange && onCompletedChange(e.target.checked);
-  };
+  const handleColorChanged = () => {};
+  const onDelete = () => {};
+  const dispatch = useDispatch();
 
-  const handleColorChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    onColorChange && onColorChange(e.target.value);
+  const handleCompletedChanged = () => {
+    dispatch({ type: "todos/todoToggled", payload: todo.id });
   };
 
   const colorOptions = availableColors.map((c) => (
@@ -37,7 +101,6 @@ const TodoListItem = ({
       {capitalize(c)}
     </option>
   ));
-
   return (
     <li>
       <div className="view">
@@ -60,9 +123,21 @@ const TodoListItem = ({
             <option value=""></option>
             {colorOptions}
           </select>
-          <button className="destroy" onClick={onDelete}>
-            {/* <TimesSolid /> */}
-            <Image src={svgTimesSolid} alt="times-solid" />
+          <button
+            className="destroy"
+            onClick={onDelete}
+            style={{
+              alignItems: "center",
+              padding: "0px",
+              marginBottom: "0px",
+            }}
+          >
+            <Image
+              src={svgTimesSolid}
+              alt="times-solid"
+              width={21}
+              height={30}
+            />
           </button>
         </div>
       </div>
